05fb70ec08065a6059258b6c39bd3933466d635c
marcocareddu <saldumaccu@tuta.io>
2023-06-16 15:41:09 +0200

    JS - Added Method toggleClassColor

--- js/script.js
+++ js/script.js
@@ -8,7 +8,13 @@ const app = createApp({
             title: 'Titolone megamagnifico',
             image: 'https://picsum.photos/200',
             alt: 'Foto a caso',
-            text: `Roba a caso giusto per vedere se funziona`
+            text: `Roba a caso giusto per vedere se funziona`,
+            classColor: 'text-danger'
+        }
+    },
+    methods: {
+        toggleClassColor() {
+            this.classColor === 'text-danger' ? this.classColor = 'text-success' : this.classColor = 'text-danger';
         }
     }
 });
