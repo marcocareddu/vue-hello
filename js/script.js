f60e4cc4ae23310db9615b230aa0c5ea7f1edb70
marcocareddu <saldumaccu@tuta.io>
2023-06-16 14:53:25 +0200

    JS - Display data in DOM

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 console.log('Vue OK', Vue);
 
-const { crateApp } = Vue;
+const { createApp } = Vue;
 
 const app = createApp({
     data() {
         return {
-
+            title: 'Titolone megamagnifico ultrapower',
+            image: 'https://picsum.photos/200',
+            alt: 'Foto a caso'
         }
     }
 });
+
+// mount
+app.mount('#root');
